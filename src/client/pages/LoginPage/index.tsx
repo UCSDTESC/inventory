b508cf0975d6dee0eadf7ff4a4c28383d5e76aef
Subhankar Panda <s1panda@ucsd.edu>
2020-03-28 18:34:40 -0400

    FE Firebase

--- src/client/pages/LoginPage/index.tsx
+++ src/client/pages/LoginPage/index.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '~/components/Button';
 import { useFirebase } from '~/firebase';
+import { useHistory } from 'react-router-dom';
 
 const LoginPage: React.FunctionComponent = () => {
   const firebase = useFirebase();
-  
+  const [error, setError] = useState<string>(null);
+  const history = useHistory();
+
   async function onLogin() {
-    const x = await firebase.signInWithPopup();
-    console.log(x);
+    const res = await firebase.signInWithPopup();
+
+    if (res.user) {
+      const [, domain] = res.user.email.split('@')
+      if (domain.trim() !== 'tesc.ucsd.edu') {
+        await logout();
+        setError('You tried logging in with a non tesc.ucsd.edu account.')
+      } else {
+        history.push('/admin');
+      }
+    } else {
+      setError('Something went wrong!')
+    }
+  }
+
+  async function logout() {
+    await firebase.logout();
   }
 
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-6">
+          <div className="w-100 text-danger text-center">{error}</div>
           <Button onClick={onLogin} className="w-auto d-flex align-items-center justify-content-center">
             <img 
              className="img-fluid mr-3"
