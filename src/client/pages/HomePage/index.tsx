dc187f2904060e514a9a6357bce5f80b83a85324
Caitlyn M Liu <caitmliu@gmail.com>
2020-03-28 20:51:01 -0700

    [Home]: Added fields firstName, lastName, email. Data validation using Yup

--- src/client/pages/HomePage/index.tsx
+++ src/client/pages/HomePage/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 import styled from 'styled-components';
 import Button from '~/components/Button';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-
 
 const ErrorMsg = styled(ErrorMessage)`
   color: red;
@@ -12,22 +13,23 @@ const ErrorMsg = styled(ErrorMessage)`
 const HomePage: React.FunctionComponent = () => {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-6">
-          <h1>TESC Inventory</h1>
+      <div className='d-flex flex-column align-items-center'>
+        <h1>TESC Inventory</h1>
+        <p>~ some text here that introduces what this is ~</p>
+        <div>
           <Formik
-            initialValues={{ name:'', email: '', password: '' }}
-            validate={values => {
-              const errors = {};
-              if (!values.email) {
-                errors.email = ' required ';
-              } else if (
-                !/^[A-Z0-9._%+-]+@ucsd+\.edu$/i.test(values.email)
-              ) {
-                errors.email = 'Invalid ucsd.edu email address';
-              }
-              return errors;
-            }}
+            initialValues={{ firstName: '', lastName: '', email: '' }}
+            validationSchema={Yup.object({
+              firstName: Yup.string()
+                .max(30, 'must be 30 characters or less')
+                .required('required'),
+              lastName: Yup.string()
+                .max(30, 'must be 30 characters or less')
+                .required('required'),
+              email: Yup.string()
+                .matches(/^[A-Z0-9._%+-]+@ucsd+\.edu$/i, 'invalid ucsd.edu email')
+                .required('required'),
+            })}
             onSubmit={(values, { setSubmitting }) => {
               setTimeout(() => {
                 alert(JSON.stringify(values, null, 2));
@@ -35,25 +37,25 @@ const HomePage: React.FunctionComponent = () => {
               }, 400);
             }}
           >
-            {({ isSubmitting }) => (
-              <Form>
-                <div>
-                  <span>Name </span>
-                  <Field type="name" name="name" />
-                  <ErrorMessage name="name" component="div" />
-                </div>
-                <div>
-                  <span>Email (your ucsd email) </span>
-                  <Field type="email" name="email" />
-                  <ErrorMsg name="email" component="span" />
-                </div>
-
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-      </Formik>
+            <Form className='d-flex flex-column'>
+              <div>
+                <span>First Name </span>
+                <Field name="firstName" type="text" />
+                <ErrorMsg name="firstName" component='span'/>
+              </div>
+              <div>
+                <span>Last Name </span>
+                <Field name="lastName" type="text" />
+                <ErrorMsg name="lastName" component='span'/>
+              </div>
+              <div>
+                <span>Email Address </span>
+                <Field name="email" type="email" />
+                <ErrorMsg name="email" component='span'/>
+              </div>
+              <button className='align-self-center' type="submit">Submit</button>
+            </Form>
+          </Formik>
         </div>
       </div>
 
