d78f0dc7b804e3e5d23bf74c5bda3ca44ddf0e94
Caitlyn M Liu <caitmliu@gmail.com>
2020-04-04 15:38:30 -0700

    [CheckOutRequest]: oopsie forgot to rename the controller elswhere

--- src/server/api/controllers/index.ts
+++ src/server/api/controllers/index.ts
@@ -1,5 +1,5 @@
 import ItemsController from './admin/ItemsController';
-import UserItemsController from './user/ItemsController';
+import UserRequestController from './user/RequestController';
 
 export const AdminControllers = [ItemsController];
-export const UserControllers = [UserItemsController];
+export const UserControllers = [UserRequestController];
