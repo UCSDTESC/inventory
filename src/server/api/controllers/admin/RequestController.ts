1e159d9a35c8d2405b562b95608d80e30a9be43d
enlinucsd <57239515+enlinucsd@users.noreply.github.com>
2020-10-27 21:54:52 -0700

    Update RequestController.ts
    added get request function

--- src/server/api/controllers/admin/RequestController.ts
+++ src/server/api/controllers/admin/RequestController.ts
@@ -1,7 +1,22 @@
 import { Get, JsonController, UseBefore, Res, Req, Post, Body } from 'routing-controllers';
-
+import AdminAuthorisation from '../../middleware/AdminAuthorization';
+import { GetItemsResponse, SuccessResponse, GetRequestsResponse } from '@Shared/api/Responses'; 
+import { CreateItemRequest } from '@Shared/api/Requests';
+import RequestService from '@Services/RequestService';
+import CloudStorageService from '@Services/CloudStorageService';
+import { InventoryItem } from '@Shared/Types';
+import { FirebaseUID } from 'api/decorators/FirebaseUID';
+import { Parser } from 'json2csv';
+import { auth } from 'firebase-admin';
+import { Response, Request } from 'express';
+import Uploads from '@Config/Uploads';
 
 @JsonController('/request')
 export default class RequestController {
-  constructor() {}
+  constructor(private RequestService: RequestService, private CloudStorageService: CloudStorageService) {}
+
+  @Get()
+  async getRequests(): Promise<GetRequestsResponse> {
+    return await this.RequestService.getAllRequests();
+  }
 }
